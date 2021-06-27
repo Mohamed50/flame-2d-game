ebbdca21043dc18c905a02382d615e13093668bb
fifty <mohamedelameen50>
2021-06-27 18:22:11 +0200

    add start-button

--- lib/views/home-view.dart
+++ lib/views/home-view.dart
@@ -1,17 +1,15 @@
 import 'dart:ui';
-
 import 'package:flame/sprite.dart';
-import 'package:flutter/gestures.dart';
+import 'package:flutter/src/gestures/tap.dart';
+import 'package:flutter_game/component/start-button.dart';
 import 'package:flutter_game/game-controller.dart';
-import 'package:flutter_game/state/game-state.dart';
 
 class HomeView {
 
   final GameController controller;
   Rect titleRect;
   Sprite titleSprite;
-  Rect startRect;
-  Sprite startSprite;
+  StartButton startButton;
 
   HomeView(this.controller){
     initializeTitle();
@@ -26,22 +24,16 @@ class HomeView {
   }
 
   initializeStart(){
-    double width = controller.tileSize * 6;
-    double height = controller.tileSize * 3;
-    startRect = Rect.fromLTWH(controller.screenSize.width / 2 - width / 2, controller.screenSize.height * 0.8, width, height);
-    startSprite = Sprite('ui/start-button.png');
+    startButton = StartButton(controller);
   }
 
   void render(Canvas canvas){
     titleSprite.renderRect(canvas, titleRect);
-    startSprite.renderRect(canvas, startRect);
+    startButton.render(canvas);
   }
 
-  void update(){}
-
-  void onTapDown(TapDownDetails details){
-    if(startRect.contains(details.globalPosition))
-      controller.gameState = GameState.Playing;
+  void onTapDown(TapDownDetails details) {
+    startButton.onTapDown(details);
   }
 
 }
