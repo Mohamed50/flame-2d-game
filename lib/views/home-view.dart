87397fb455a857b80a160b7992e3adc3d4aaf2a2
fifty <mohamedelameen50>
2021-06-27 18:37:02 +0200

    add credit-button and help-button

--- lib/views/home-view.dart
+++ lib/views/home-view.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 import 'package:flame/sprite.dart';
-import 'package:flutter/src/gestures/tap.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter_game/component/credit-button.dart';
+import 'package:flutter_game/component/help-button.dart';
 import 'package:flutter_game/component/start-button.dart';
 import 'package:flutter_game/game-controller.dart';
 
@@ -10,10 +12,12 @@ class HomeView {
   Rect titleRect;
   Sprite titleSprite;
   StartButton startButton;
+  HelpButton helpButton;
+  CreditsButton creditsButton;
 
   HomeView(this.controller){
     initializeTitle();
-    initializeStart();
+    initializeButtons();
   }
 
   initializeTitle(){
@@ -23,17 +27,23 @@ class HomeView {
     titleSprite = Sprite('branding/title.png');
   }
 
-  initializeStart(){
+  initializeButtons(){
     startButton = StartButton(controller);
+    helpButton = HelpButton(controller);
+    creditsButton = CreditsButton(controller);
   }
 
   void render(Canvas canvas){
     titleSprite.renderRect(canvas, titleRect);
     startButton.render(canvas);
+    helpButton.render(canvas);
+    creditsButton.render(canvas);
   }
 
   void onTapDown(TapDownDetails details) {
     startButton.onTapDown(details);
+    helpButton.onTapDown(details);
+    creditsButton.onTapDown(details);
   }
 
 }
