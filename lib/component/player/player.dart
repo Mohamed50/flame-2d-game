8ec3a8c8b1b4f11a823d1073f72aedad01371ebb
fifty <mohamedelameen50>
2021-06-28 21:37:35 +0200

    add pizza steve as player character

--- lib/component/player/player.dart
+++ lib/component/player/player.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_game/data/pref-manager.dart';
 import 'package:flutter_game/game-controller.dart';
@@ -9,22 +10,23 @@ class Player {
   int maxHealth;
   int currentHealth;
   Rect playerRect;
+  Sprite playerSprite;
   bool isDead = false;
 
   Player(this.controller) {
     maxHealth = currentHealth = 400;
-    final size = controller.tileSize * 1.5;
+    final size = controller.tileSize * 3;
     playerRect = Rect.fromLTWH(
       controller.screenSize.width / 2 - size / 2,
       controller.screenSize.height / 2 - size / 2,
       size,
       size,
     );
+    playerSprite = Sprite('player/steve.png');
   }
 
   void render(Canvas canvas){
-    Paint paint = Paint()..color = Colors.amber;
-    canvas.drawRect(playerRect, paint);
+    playerSprite.renderRect(canvas, playerRect);
   }
 
   Future<void> update(double t) async {
